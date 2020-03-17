1434cf0defa0f4eb2e260596479b2031640bf9ce
Ryan Culpepper <ryanc@racket-lang.org>
2020-03-17 05:10:57 +0100

    add copyright, CC-BY-SA 4 license

--- site.rkt
+++ site.rkt
@@ -1,5 +1,8 @@
-#lang racket/base
+#lang at-exp racket/base
 (require racket/runtime-path
+         racket/class
+         racket/list
+         racket/date
          jeremiah/config
          net/url)
 
@@ -17,3 +20,30 @@
 
 (tag-uri-entity "rmculpepper.github.io,2020")
 (tag-uri-prefix "blog")
+
+(extra-html
+ (lambda (location page)
+   (case location
+     [(end-content)
+      (define copyright-year
+        (or (and (send page is-page-type? 'post) (send page get-year))
+            (let* ([posts (send (the-site) get-posts)]
+                   [post-years (filter string? (map (lambda (p) (send p get-year)) posts))]
+                   [now-year (number->string (date-year (current-date)))]
+                   [max-year (argmax string->number (cons now-year post-years))]
+                   [min-year (argmin string->number (cons now-year post-years))])
+              (if (equal? min-year max-year) min-year (format "~a&ndash;~a" min-year max-year)))))
+      @list{<hr />
+            <div>
+            <div style="padding: 0.5em; float: left">
+            <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+              <img alt="Creative Commons License" style="border-width:0"
+                   src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
+            </div>
+            <div>
+            Copyright @|copyright-year| Ryan Culpepper.
+            This work is licensed under a
+            <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+            Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+            </div>
+            }])))
